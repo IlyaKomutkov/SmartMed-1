0fb89a2e62f7fa538ad95f99d6f5a5a8135e4f64
Ilya Komutkov <iskomutkov@edu.hse.ru>
2020-11-28 23:11:17 +0300

    kstest, Shapiro test, f_oneway test added

--- backend/modules/models/BioquivalenceMathsModel.py
+++ backend/modules/models/BioquivalenceMathsModel.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 from sklearn.metrics import auc
+from scipy import stats
 
 
 class BioquivalenceMathsModel:
@@ -10,3 +11,12 @@ class BioquivalenceMathsModel:
 
     def get_log_array(x: np.array) -> np.array:
         return np.log(x)
+
+    def get_kstest(x: np.array) -> tuple:
+    	return stats.kstest(x, 'norm')
+
+    def get_shapiro(x: np.array) -> tuple:
+    	return stats.shapiro(x)
+
+    def get_f(x: np.array, y: np.array) -> tuple:
+    	return stats.f_oneway(x, y)
